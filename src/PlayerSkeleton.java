cc751c72109ecd58c393b8611d98465c02a22eef
Nicholas Lum <nicholaslum444@hotmail.com>
2015-04-18 01:17:11 +0800

    Implemented the multithread learning.
    1. they share a central array of values. they all commit to it and take from
    it as well.
    2.i renamed the original good weights with an extra s behind. the current
    counts and weights are from the parallel learning.

--- src/PlayerSkeleton.java
+++ src/PlayerSkeleton.java
@@ -1,5 +1,22 @@
-public class PlayerSkeleton {
+interface PlayerListener {
+	void setRunning(int i);
+	void reportFinished();
+}
 
+public class PlayerSkeleton implements PlayerListener {
+	
+	volatile static int running = 2;
+	
+
+	int NUM_OF_PLAYERS = 80;
+	int TIMES_TO_TRAIN = 1000;
+	boolean SHOW_BOARD = false;
+	
+	// change this to false to only run once for "exploitation"
+	final static boolean TRAINING_MODE = true;
+	
+	static int trained = 0;
+	
 	public Trainer trainer = new Trainer();	
 
 	//implement this function to have a working system
@@ -8,17 +25,65 @@ public class PlayerSkeleton {
 		return bestMove;
 	}
 	
-	public static void main(String[] args) {
-		
-		int NUM_OF_PLAYERS = 4;
-		boolean SHOW_BOARD = true;
-		
-		for (int i = 0; i < NUM_OF_PLAYERS; i++) {
-			
-			// play in a new thread
-			new Thread(new Player(i, SHOW_BOARD)).start();
-			
+	@Override
+	public synchronized void setRunning(int i) {
+		running = i;
+	}
+	
+	@Override
+	public synchronized void reportFinished() {
+		// threads will report finished when done.
+		// this decrements the number of running threads.
+		if (TRAINING_MODE) {
+			running -= 1;
+			if (running == 0) {
+				System.out.println("save files");
+				CountsAndValues.saveAndExitInstance();
+				
+				// when no more running threads,
+				// go to next iteration
+				train();
+			}
+		} else {
+			System.out.println("save files");
+			CountsAndValues.saveAndExitInstance();
 		}
+	}
+	
+	public void train() {
+
+		System.out.println("read files");
+		CountsAndValues.getInstance();
+		
+		// training mode on: we train for x times.
+		if (TRAINING_MODE) {
+			// if we have trained to the max,
+			// then stop training and reset the count. 
+			if (trained >= TIMES_TO_TRAIN) {
+				trained = 0;
+				return;
+			}
+			
+			System.out.println("Training set " + trained);
+			
+			// else we can still train.
+			// remember the number of running threads,
+			// then run the threads.
+			setRunning(NUM_OF_PLAYERS);
+			for (int i = 0; i < NUM_OF_PLAYERS; i++) {
+				new Thread(new Player(i, SHOW_BOARD, this)).start();
+			}
+			
+			// increment the number of times we trained
+			trained++;
+		} else {
+			// training mode off: just play once.
+			new Thread(new Player(0, true, this)).start();
+		}
+	}
+	
+	public static void main(String[] args) {
+		new PlayerSkeleton().train();
 	}
 	
 }
@@ -27,16 +92,18 @@ class Player implements Runnable {
 	
 	int i;
 	boolean q;
+	PlayerListener p;
 	
-	public Player(int i, boolean q) {
+	public Player(int i, boolean q, PlayerListener p) {
 		this.i = i;
 		this.q = q;
+		this.p = p;
 	}
 
 	@Override
 	public void run() {
-		CountsAndValues.getInstance();
 		System.out.println("Starting thread " + i);
+		
 		State s = new State();
 		TFrame t = q ? new TFrame(s) : null;
 		PlayerSkeleton p = new PlayerSkeleton();
@@ -48,11 +115,9 @@ class Player implements Runnable {
 			}
 			//System.out.println(s.getRowsCleared());
 			if (s.getRowsCleared() % 1000 == 0) {
-				System.out.println("Thread " + i + " " + s.getRowsCleared());
+				//System.out.println("Thread " + i + " " + s.getRowsCleared());
 			}
 		}
-		
-		CountsAndValues.saveAndExitInstance();
 		
 		System.out.println("Thread " + i + " ended");
 		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
@@ -60,6 +125,8 @@ class Player implements Runnable {
 		if (q) {
 			t.dispose();
 		}
+		
+		p.reportFinished();
 	}
 	
 }
